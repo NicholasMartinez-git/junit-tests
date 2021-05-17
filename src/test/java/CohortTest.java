6d97a729bfb9adf17858585c80163718e8330ae5
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-05-17 09:49:52 -0500

    completed exercise. Need to refactor

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -1,5 +1,6 @@
-
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class CohortTest {
     @Test
@@ -24,55 +25,10 @@ public class CohortTest {
         mtnz.addStudent(nic);
         mtnz.addStudent(matt);
         mtnz.addStudent(jon);
-    }
-}
 
-//import org.junit.Before;
-//import org.junit.Test;
-//import static org.junit.Assert.*;
-//
-//public class CohortTest {
-//
-//    Cohort emptyCohort;
-//    Cohort cohortWOne;
-//    Cohort cohortWMany;
-//
-//    @Before
-//    public void init(){
-//
-//        emptyCohort = new Cohort();
-//        cohortWOne = new Cohort();
-//        cohortWMany = new Cohort();
-//
-//        Student fer = new Student(10L, "Fer");
-//        Student david = new Student(20L, "David");
-//
-//        fer.addGrade(100);
-//        fer.addGrade(40);
-//
-//        david.addGrade(80);
-//        david.addGrade(90);
-//        cohortWOne.addStudent(fer);
-//        cohortWMany.addStudent(fer);
-//        cohortWMany.addStudent(david);
-//
-//    }
-//
-//    @Test
-//    public void testIfAddStudentWorks(){
-//        assertEquals(1, cohortWOne.getStudents().size());
-//        assertEquals(2, cohortWMany.getStudents().size());
-//    }
-//
-//    @Test
-//    public void testIfGetStudentsWork(){
-//        assertEquals(10, cohortWMany.getStudents().get(0).getId());
-//        assertEquals(20, cohortWMany.getStudents().get(1).getId());
-//    }
-//
-//    @Test
-//    public void testIfCohortAvgWorks(){
-//        assertEquals(70, cohortWOne.getCohortAverage(), 0.5);
-//        assertEquals(77.5, cohortWMany.getCohortAverage(), 0.5);
-//    }
-//
+        assertEquals(3, mtnz.getStudents().size());
+        assertEquals(1, mtnz.getStudents().get(0).getId());
+        assertEquals(89, mtnz.getCohortAverage(), 0.5);
+    }
+
+}
